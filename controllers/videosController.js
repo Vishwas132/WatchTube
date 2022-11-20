e218c0b44c93899ebce60e4543066af38de3b650
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2022-11-20 22:38:37 +0530

    Implement videosController

---
+++ controllers/videosController.js
@@ -0,0 +1,79 @@
+import * as video from "./../services/videos.js";
+
+const uploadVideo = async (req, res) => {
+  try {
+    const videoObj = await video.newVideo(req.body);
+    return res.status(200).json(videoObj);
+  } catch (error) {
+    console.log("error", error);
+    throw error;
+  }
+};
+
+const getVideos = async (req, res) => {
+  try {
+    const videosObj = await video.getAllVideos();
+    if (videosObj === undefined) throw Error("No video");
+    return res.status(200).json(videosObj);
+  } catch (error) {
+    console.log("error", error);
+    return res.status(404).json(error.message);
+  }
+};
+
+const getVideoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const videoObj = await video.getVideo(id);
+    if (videoObj === undefined) throw Error("No video");
+    return res.status(200).json(videoObj);
+  } catch (error) {
+    console.log("error", error);
+    return res.status(404).json(error.message);
+  }
+};
+
+const deleteVideoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const videoObj = await video.deleteVideo(id);
+    if (!videoObj) throw Error("No video");
+    return res.status(200).json(`Video with id=${id} deleted`);
+  } catch (error) {
+    console.log("error", error);
+    return res.status(404).json(error.message);
+  }
+};
+
+const likeVideoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const likedObj = await video.likeVideo(id);
+    if (!likedObj?.id) throw Error("No video");
+    return res.status(200).json(`Video liked`);
+  } catch (error) {
+    console.log("error", error);
+    return res.status(404).json(error.message);
+  }
+};
+
+const dislikeVideoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const likedObj = await video.dislikeVideo(id);
+    if (!likedObj?.id) throw Error("No video");
+    return res.status(200).json(`Video disliked`);
+  } catch (error) {
+    console.log("error", error);
+    return res.status(404).json(error.message);
+  }
+};
+
+export {
+  uploadVideo,
+  getVideos,
+  getVideoById,
+  deleteVideoById,
+  likeVideoById,
+  dislikeVideoById,
+};
